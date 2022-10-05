1ea7a87391d227379deb869138d6ad9f77a027d9
dg-alt <dagusar@mail.ru>
2022-10-05 14:16:31 +0300

    #2
    header creating and testing

--- Lab_Gusarov_metodi/Lab_Gusarov_metodi/Gusarov_viz.h
+++ Lab_Gusarov_metodi/Lab_Gusarov_metodi/Gusarov_viz.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::fstream;
+using std::cout;
+
+static class Gusarov_viz
+{
+public:
+	static void EncryptFileA(const string& key, const string& nameFileText, const string& nameFileSave);
+	static void DecryptFileA(const string& key, const string& nameFileText, const string& nameFileSave);
+};
+
+
